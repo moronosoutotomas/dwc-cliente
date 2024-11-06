ce1df3b8aefc6741fa998657455cfe2270e6f029
Tomás Clase <moronosoutotomas.sangrina@gmail.com>
2024-11-06 17:55:37 +0100

    update

--- Ejercicios/juego de la rana/script.js
+++ Ejercicios/juego de la rana/script.js
@@ -1,6 +1,13 @@
+// variables
+//const html = document.getElementsByTagName('html');
 const rana = document.getElementById('rana');
 const puntuacion = document.getElementById('puntuacion');
-const MAX_SALTOS = 20;
+let saltosRestantes = document.getElementById('saltosRestantes'); // desde 20
+let tiempoSalto = 2000; // en milisegundos
+
+// listeners
+rana.addEventListener('click', updateMarcador());
+//html.addEventListener('load', startMarcador());
 
 function puntuar() {
   // puntua al capturar la rana
@@ -8,8 +15,21 @@ function puntuar() {
 
 function salto() {
   // la rana salta al capturarla
+  tiempoSalto -= 250;
 }
 
-function nuevasCoords() {
+function rngCoords() {
   // genero nuevas coords aleatoriamente
 }
+
+function redimensionarRana() {
+  rana.width -= 20;
+  rana.height -= 20;
+}
+
+function updateMarcador() {
+  puntuacion.innerText = punt_inicial++;
+  saltosRestantes.innerText = max_saltos--;
+}
+
+setInterval(redimensionarRana(), tiempoSalto);
