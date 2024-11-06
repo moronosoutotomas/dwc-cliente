bc0ef34e66b746b40332b6b5da973ab0a72699b2
Tomás Clase <moronosoutotomas.sangrina@gmail.com>
2024-11-06 16:53:38 +0100

    renombradas varias carpetas

---
+++ Ejercicios/juego de la rana/script.js
@@ -0,0 +1,15 @@
+const rana = document.getElementById('rana');
+const puntuacion = document.getElementById('puntuacion');
+const MAX_SALTOS = 20;
+
+function puntuar() {
+  // puntua al capturar la rana
+}
+
+function salto() {
+  // la rana salta al capturarla
+}
+
+function nuevasCoords() {
+  // genero nuevas coords aleatoriamente
+}
