0f0a356b6de0695d554025e9a8a4e5235785bc75
Tomás Fedora <moronosoutotomas.sangrina@gmail.com>
2024-12-10 22:50:08 +0100

    terminado el test exitosamente w0w

--- test/script.js
+++ test/script.js
@@ -18,12 +18,12 @@ let farenheit = 0;
 
 // Fn que convierte un dato numérico parametrizado a grados Celsius
 function aCelsius(kelvins) {
-	return Math.round(kelvins - kelvin);
+	return (kelvins - kelvin).toFixed(2);
 }
 
 // Fn que convierte un dato numérico parametrizado a grados Farenheit
 function aFarenheit(kelvins) {
-	return Math.round(1.8 * (kelvins - kelvin) + 32);
+	return (1.8 * (kelvins - kelvin) + 32).toFixed(2);
 }
 
 // Metemos los valores en un array y eliminamos los espacios
@@ -31,22 +31,112 @@ const arrayInput = input.split(',').map(x => x.trim());
 
 // Fn que convierte cada uno de los datos a Farenheit
 function calcDataFar() {
-	document.getElementById('output').innerHTML = '';
+	const output = document.getElementById('output');
+	output.innerHTML = '';
 
 	let far = arrayInput.map(x => aFarenheit(x));
-	let resultado = document.createTextNode(far);
-	let parrafo = document.createElement('p');
-	parrafo.appendChild(resultado);
-	document.getElementById('output').appendChild(parrafo);
+
+	// Ordenamos en caso de que esté el checkbox marcado
+	if (document.getElementById('orden').checked) {
+		far.sort((a, b) => {
+			return a - b;
+		});
+	}
+
+	// Metemos cada posición del array en un párrafo independiente para
+	// poder aplicarle el estilo requerido
+	far.forEach(element => {
+		let color = '';
+		let bgColor = 'white';
+		let resultado = document.createTextNode(element);
+		let span = document.createElement('span');
+		span.appendChild(resultado);
+
+		// En caso de que se haya seleccionado el modo "alto contraste"
+		// mostraremos los resultados en ese modo, por defecto se verá
+		// en modo coloreado.
+		if (
+			document.querySelector('input[name="radio"]:checked').value ==
+			'alto-contraste'
+		) {
+			color = 'white';
+			bgColor = 'black';
+		} else {
+			if (element < 0) {
+				color = 'purple';
+			} else if (element < 100) {
+				color = 'blue';
+			} else if (element < 200) {
+				color = 'green';
+			} else if (element < 300) {
+				color = 'yellow';
+			} else if (element < 400) {
+				color = 'orange';
+			} else {
+				color = 'red';
+			}
+		}
+
+		span.style.padding = '.5em';
+		span.style.color = color;
+		output.style.backgroundColor = bgColor;
+
+		output.appendChild(span);
+	});
 }
 
 // Fn que convierte cada uno de los datos a Celsius
 function calcDataCel() {
-	document.getElementById('output').innerHTML = '';
+	const output = document.getElementById('output');
+	output.innerHTML = '';
 
 	let cel = arrayInput.map(x => aCelsius(x));
-	let resultado = document.createTextNode(cel);
-	let parrafo = document.createElement('p');
-	parrafo.appendChild(resultado);
-	document.getElementById('output').appendChild(parrafo);
+
+	// Ordenamos en caso de que esté el checkbox marcado
+	if (document.getElementById('orden').checked) {
+		cel.sort((a, b) => {
+			return a - b;
+		});
+	}
+
+	// Metemos cada posición del array en un párrafo independiente para
+	// poder aplicarle el estilo requerido
+	cel.forEach(element => {
+		let color = '';
+		let bgColor = 'white';
+		let resultado = document.createTextNode(element);
+		let span = document.createElement('span');
+		span.appendChild(resultado);
+
+		// En caso de que se haya seleccionado el modo "alto contraste"
+		// mostraremos los resultados en ese modo, por defecto se verá
+		// en modo coloreado.
+		if (
+			document.querySelector('input[name="radio"]:checked').value ==
+			'alto-contraste'
+		) {
+			color = 'white';
+			bgColor = 'black';
+		} else {
+			if (element < 0) {
+				color = 'purple';
+			} else if (element < 100) {
+				color = 'blue';
+			} else if (element < 200) {
+				color = 'green';
+			} else if (element < 300) {
+				color = 'yellow';
+			} else if (element < 400) {
+				color = 'orange';
+			} else {
+				color = 'red';
+			}
+		}
+
+		span.style.padding = '.5em';
+		span.style.color = color;
+		output.style.backgroundColor = bgColor;
+
+		output.appendChild(span);
+	});
 }
