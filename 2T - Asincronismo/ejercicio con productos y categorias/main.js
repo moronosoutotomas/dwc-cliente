4927c46cab9efc2782b0b5717f0b8efd66f8249d
Tomás Clase <moronosoutotomas.sangrina@gmail.com>
2025-01-27 12:35:38 +0100

    update

--- 2T - Asincronismo/ejercicio con productos y categorias/main.js
+++ 2T - Asincronismo/ejercicio con productos y categorias/main.js
@@ -15,11 +15,55 @@ nombre de la categoría.
 
 
 // Obtener datos JSON
+let listadoProductos = [];
+
+fetch('productos.json')
+  .then(response => {
+    // Comprobamos si la respuesta es correcta
+    if (!response.ok) {
+      throw new Error('Error en la respuesta: ' + response.status);
+    }
+    // Parseamos la respuesta a JSON
+    return response.json();
+  })
+  .then(data => {
+    // Aqui manejamos el objeto JS resultante
+    listadoProductos = data;
+
+    //console.log(listadoProductos);
+  })
+  .catch(error => {
+    // Manejamos cualquier error durante la petición
+    console.error('Ha ocurrido un error: ', error);
+  });
 
 
 
 // Obtener datos XML
+let listadoCategorias = [];
 
+fetch('categorias.xml')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error en la respuesta: ' + response.status);
+    }
+    // Creamos una instancia del parser
+    const parser = new DOMParser();
+
+    // Parseamos la cadena con su debido formato
+    const xmlDoc = parser.parseFromString(response, 'text/html');
+
+    //console.log(xmlDoc);
+  })
+  .then(data => {
+    listadoProductos = data;
+  })
+  .catch(error => {
+    console.error('Ha ocurrido un error: ', error);
+  });
 
 
 // Combinar info
+for (let i = 0; i < listadoCategorias.length; i++) {
+
+}
