7ef750e2b89f01ea8af4f6ccc97ea6f0df2b5232
Tomás Clase <moronosoutotomas.sangrina@gmail.com>
2025-01-27 12:15:51 +0100

    empezado el ejercicio de productos y categorias

---
+++ 2T - Asincronismo/ejercicio con productos y categorias/main.js
@@ -0,0 +1,25 @@
+/*
+Crear un archivo JavaScript (main.js) que obtenga los datos JSON y XML y que
+combine la información:
+
+• Por cada producto, buscar la categoría correspondiente en el XML.
+
+• Crear de forma dinámica una tabla (o la estructura HTML que se considere,
+aunque se recomienda tabla) mostrando el nombre del producto y el
+nombre de la categoría.
+
+• Generar la tabla dinámicamente.
+
+• Cada elemento de cabecera permitirá ordenar la tabla por dicho elemento.
+*/
+
+
+// Obtener datos JSON
+
+
+
+// Obtener datos XML
+
+
+
+// Combinar info
